e8757736d1aa9dd9b82fb4ccbe0ba32b28d0612c
franc <franc@LAPTOP-G299TFEB>
2019-04-19 23:22:19 +0200

    .

---
+++ bocdemo/src/main/java/com/fdorval/bocdemo/BocdemoApplication.java
@@ -0,0 +1,13 @@
+package com.fdorval.bocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocdemoApplication.class, args);
+	}
+
+}
