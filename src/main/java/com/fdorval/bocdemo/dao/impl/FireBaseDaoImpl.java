e9532e4af4428b4de3ead49db1a0121a74b3cf5b
francois-dorval <francois.dorval@gmail.com>
2019-04-28 23:21:51 +0200

    test profile

--- src/main/java/com/fdorval/bocdemo/dao/impl/FireBaseDaoImpl.java
+++ src/main/java/com/fdorval/bocdemo/dao/impl/FireBaseDaoImpl.java
@@ -21,7 +21,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-@Profile("!test")
+@Profile("default")
 @Repository
 public class FireBaseDaoImpl implements FireBaseDao {
 
