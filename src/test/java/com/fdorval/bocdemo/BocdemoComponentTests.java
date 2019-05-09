c632a9019bd873c9d7acef63e60448f190d6b4bd
francois-dorval <francois.dorval@gmail.com>
2019-05-10 00:08:06 +0200

    test composant

---
+++ src/test/java/com/fdorval/bocdemo/BocdemoComponentTests.java
@@ -0,0 +1,52 @@
+package com.fdorval.bocdemo;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fdorval.bocdemo.business.BocBusiness;
+import com.fdorval.bocdemo.dao.FireBaseDao;
+import com.fdorval.bocdemo.dao.stub.FireBaseDaoStub;
+import com.fdorval.bocdemo.model.Student;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BocdemoComponentTests {
+
+	Logger LOG = LoggerFactory.getLogger(BocdemoComponentTests.class);
+
+	 @LocalServerPort
+	    private int port;
+
+	    @Autowired
+	    private TestRestTemplate restTemplate;
+	    
+	    @Test
+	    public void shouldReturnStudents() throws Exception {
+	        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/students",
+	                String.class)).contains("R2D2");
+	    }
+	    
+	    @Test
+	    public void wrongUrlIn404() throws Exception {
+	        assertThat(this.restTemplate.getForEntity("http://localhost:" + port + "/kamoulox",
+	                String.class).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	    }
+}
